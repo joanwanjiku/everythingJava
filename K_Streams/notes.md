b1cd0785956ccdea24b4d7b8c425698da1732ba2
joanwanjiku <joanevans18@gmail.com>
2025-05-15 02:12:20 -0400

    stream methods

--- K_Streams/notes.md
+++ K_Streams/notes.md
@@ -1,3 +1,11 @@
 # Stream
 - A sequence of elements supporting sequential and parallel operations.
 - They make the code to process data uniform, consistent, and predictable.
+- Stream pipelines are composed of a sequence of intermediate operations.
+- Stream pipelines are executed using a terminal operation. It is the last operation in the pipeline and is required.
+- After invoke the terminal operation, the stream is closed.
+
+## Stream Sources
+1. Collections
+2. Arrays
+- An intermiate operation can be recognized by it's signature because it returns a stream. this doesn't mean that the element type of the stream can't change.
--- K_Streams/src/Main.java
+++ K_Streams/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,12 +26,77 @@ public class Main {
         });
 //        System.out.println(bingoPool);
         System.out.println("\n----------------------------------");
-        bingoPool.stream()
+        var tempStream = bingoPool.stream()
                 .limit(15)
                 .filter(s -> s.indexOf('G') == 0 || s.indexOf('O') == 0)
                 .map(s -> s.charAt(0) + "-" + s.charAt(1))
-                .sorted()
+                .sorted();
+//                .forEach(System.out::println);
+//        tempStream.forEach(System.out::println); closes the stream
+        tempStream.forEach(s -> System.out.println(s.toLowerCase() + " "));
+
+        System.out.println("\n----------------------------------");
+        String[] strings = {"A", "B", "C", "D", "E", "F", "G"};
+        var firstStream = Arrays.stream(strings)
+                .sorted(Comparator.reverseOrder());
+        var secondStream = Stream.of("J", "K", "L", "M", "N", "O", "P")
+                .map(String::toLowerCase);
+        Stream.concat(firstStream, secondStream)
+                .map(s -> s + " -")
+                .forEach(System.out::println);
+        System.out.println("\n----------------------------------");
+
+        Map<Character, int[]> myMap = new LinkedHashMap<>();
+        int bingoIndex = 1;
+        for (char c : "BINGO".toCharArray()) {
+            int[] numbers = new int[15];
+            int labelNo = bingoIndex;
+            Arrays.setAll(numbers, i -> i + labelNo);
+            myMap.put(c, numbers);
+            bingoIndex += 15;
+        }
+        myMap.entrySet()
+                .stream()
+                .map(e -> e.getKey() + " has range "+ e.getValue()[0] + " to " + e.getValue()[e.getValue().length - 1])
+                .forEach(System.out::println);
+        System.out.println("\n----------------------------------");
+        Random random = new Random();
+        Stream.generate(() -> random.nextInt(2))
+                .limit(10)
+                .forEach(System.out::print);
+        System.out.println("\n----------------------------------");
+        IntStream.iterate(1, i -> i + 1)
+                .filter(Main::isPrime)
+                .limit(20)
                 .forEach(System.out::println);
 
+        System.out.println("\n----------------------------------");
+        IntStream.iterate(1, i -> i + 1)
+                .limit(20)
+                .filter(Main::isPrime)
+                .forEach(System.out::println);
+
+        System.out.println("\n----------------------------------");
+        IntStream.iterate(1, n -> n <=20,i -> i + 1)
+                .filter(Main::isPrime)
+                .forEach(System.out::println);
+
+        System.out.println("\n----------------------------------");
+        IntStream.range(1,100)
+                .filter(Main::isPrime)
+                .forEach(System.out::println);
+
+
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 2){
+            return n == 2;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
